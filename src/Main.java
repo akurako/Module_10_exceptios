a7a3b65b43a9ba3a1b0d76f28396a6ee2de0493c
akurako <78310019+akurako@users.noreply.github.com>
2021-04-13 00:47:47 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         PhoneBook pb = new PhoneBook();
-       // pb.AddNew("Aleksejs", "Kurako", "+32486661433");
+        pb.AddNew("Aleksejs", "Kurako", "+32486661433");
         pb.ListAll();
         pb.ShowNumber(3);
     }
